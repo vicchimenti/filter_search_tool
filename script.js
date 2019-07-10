d41a61f3c49138e03ce48811be388d655ba1402c
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-10 11:36:58 -0700

    revert

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 <script>
 
 $(function () {
-  $('input#id_search').hideseek('.educationAbroadItemWrapper', {
-    'delay': 100,
+  $('input#id_search').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 400,
     'selector': ['h3, p'],
     'stripeRows': ['even', 'odd'],
     'noResults': '.noResultsToShow',
@@ -225,32 +225,31 @@ $(function () {
 });
 
 
-// *************** Remove Institution per Notes ********************* //
-// $(function () {
-//   $('#SelectBox-ByInstitution').quicksearch('.educationAbroadItemWrapper', {
-//     'delay': 100,
-//     'selector': '.institution > ul',
-//     'stripeRows': ['even', 'odd'],
-//     'noResults': '.noResultsToShow',
-//     'bind': 'keyup click',
-//     'minValLength': 2,
-//                  'prepareQuery': function (val) {
-//       return new RegExp(val, "i");
-//     },
-//     'testQuery': function (query, txt, _row) {
-//       return query.test(txt);
-//     },
-//     'show': function() {
-//       $(this).removeClass('hideByInstitutionDropDown');
-//     },
-//     'hide': function() {
-//       $(this).addClass('hideByInstitutionDropDown');
-//     },
-//     'onAfter': function () {
-//       anythingThere();
-//     }
-//   });
-// });
+$(function () {
+  $('#SelectBox-ByInstitution').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.institution > ul',
+    'stripeRows': ['even', 'odd'],
+    'noResults': '.noResultsToShow',
+    'bind': 'keyup click',
+    'minValLength': 2,
+                 'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByInstitutionDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByInstitutionDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
 
 
 var visibleItems = $(".educationAbroadItemWrapper");
