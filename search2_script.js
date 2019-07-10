5b09e0a45e73dfc8c13747264b37fdd947f80fc3
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-10 14:39:10 -0700

    Update search2_script.js

--- search2_script.js
+++ search2_script.js
@@ -1,8 +1,8 @@
 <script>
 
 $(function () {
-  $('input#id_search').hideseek('.educationAbroadItemWrapper', {
-    'delay': 100,
+  $('input#id_search').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 400,
     'selector': ['h3, p'],
     'stripeRows': ['even', 'odd'],
     'noResults': '.noResultsToShow',
@@ -25,11 +25,38 @@ $(function () {
 
 
 $(function () {
-  $('#SelectBox-ByTerm').quicksearch('div.educationAbroadItemWrapper', {
+  $('#SelectBox-ByDestination').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.destination > ul',
+    'stripeRows': ['even', 'odd'],
+    'noResults': '.noResultsToShow',
+    'bind': 'keyup click',
+    'minValLength': 2,
+    'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByDestinationDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByDestinationDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
+
+
+$(function () {
+  $('#SelectBox-ByTerm').quicksearch('.educationAbroadItemWrapper', {
     'delay': 100,
     'selector': '.term > ul',
     'bind': 'change',
-                'prepareQuery': function (val) {
+    'prepareQuery': function (val) {
       return new RegExp(val, "i");
     },
     'testQuery': function (query, txt, _row) {
@@ -49,11 +76,89 @@ $(function () {
 
 
 $(function () {
+  $('#SelectBox-ByProgramFee').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.programFee > ul',
+    'bind': 'change',
+    'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByProgramFeeDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByProgramFeeDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
+
+
+$(function () {
+  $('#SelectBox-ByFieldOfStudy').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.fieldOfStudy > ul',
+    'stripeRows': ['even', 'odd'],
+    'noResults': '.noResultsToShow',
+    'bind': 'keyup click',
+    'minValLength': 2,
+    'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByFieldOfStudyDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByFieldOfStudyDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
+
+
+$(function () {
+  $('#SelectBox-ByFeature').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.features > ul',
+    'stripeRows': ['even', 'odd'],
+    'noResults': '.noResultsToShow',
+    'bind': 'keyup click',
+    'minValLength': 2,
+    'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByFeatureDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByFeatureDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
+
+
+$(function () {
   $('#SelectBox-ByProvider').quicksearch('.educationAbroadItemWrapper', {
     'delay': 100,
     'selector': '.provider > ul',
     'bind': 'change',
-                'prepareQuery': function (val) {
+    'prepareQuery': function (val) {
       return new RegExp(val, "i");
     },
     'testQuery': function (query, txt, _row) {
@@ -73,11 +178,35 @@ $(function () {
 
 
 $(function () {
+  $('#SelectBox-ByLanguage').quicksearch('.educationAbroadItemWrapper', {
+    'delay': 100,
+    'selector': '.language > ul',
+    'bind': 'change',
+    'prepareQuery': function (val) {
+      return new RegExp(val, "i");
+    },
+    'testQuery': function (query, txt, _row) {
+      return query.test(txt);
+    },
+    'show': function() {
+      $(this).removeClass('hideByLanguageDropDown');
+    },
+    'hide': function() {
+      $(this).addClass('hideByLanguageDropDown');
+    },
+    'onAfter': function () {
+      anythingThere();
+    }
+  });
+});
+
+
+$(function () {
   $('#SelectBox-ByGPA').quicksearch('.educationAbroadItemWrapper', {
     'delay': 100,
     'selector': '.gpa > ul',
     'bind': 'change',
-                'prepareQuery': function (val) {
+    'prepareQuery': function (val) {
       return new RegExp(val, "i");
     },
     'testQuery': function (query, txt, _row) {
@@ -96,10 +225,39 @@ $(function () {
 });
 
 
+//  ******************* Remove Institution per Notes  *******************  //
+// $(function () {
+//   $('#SelectBox-ByInstitution').quicksearch('.educationAbroadItemWrapper', {
+//     'delay': 100,
+//     'selector': '.institution > ul',
+//     'stripeRows': ['even', 'odd'],
+//     'noResults': '.noResultsToShow',
+//     'bind': 'keyup click',
+//     'minValLength': 2,
+//                  'prepareQuery': function (val) {
+//       return new RegExp(val, "i");
+//     },
+//     'testQuery': function (query, txt, _row) {
+//       return query.test(txt);
+//     },
+//     'show': function() {
+//       $(this).removeClass('hideByInstitutionDropDown');
+//     },
+//     'hide': function() {
+//       $(this).addClass('hideByInstitutionDropDown');
+//     },
+//     'onAfter': function () {
+//       anythingThere();
+//     }
+//   });
+// });
+
+
 var visibleItems = $(".educationAbroadItemWrapper");
 
 function anythingThere() {
-  visibleItems = $('.educationAbroadItemWrapper').not('.hideByTextbox, .hideByTermDropDown, .hideByProviderDropDown, .hideByGPADropDown');
+  visibleItems = $('.educationAbroadItemWrapper').not('.hideByTextbox, .hideByDestinationDropDown, .hideByTermDropDown, .hideByProgramFeeDropDown, .hideByFieldOfStudyDropDown, .hideByFeatureDropDown, .hideByProviderDropDown, .hideByLanguageDropDown, .hideByGPADropDown');
+  // .hideByInstitutionDropDown,
 
   if( visibleItems.length == 0) {
     $( ".noResultsToShow" ).show();
