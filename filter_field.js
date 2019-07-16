400b0823b2ced80cca443b4a604e7b7a16b65124
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-16 13:47:45 -0700

    Update filter_field.js

--- filter_field.js
+++ filter_field.js
@@ -1,6 +1,6 @@
 try {
   var fieldInstructions = content.get("Instructions");
-  var fieldFieldCode = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Zone' output='normal' display_field='value' />");
+  var fieldFieldCode = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='form' name='Education Abroad Search Filters' id='2162' />");
   var fieldNoResultsMessage = content.get("No Results Message");
   var message = "";
   var fieldZone = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Zone' output='normal' display_field='value' />");
@@ -15,9 +15,9 @@ try {
 
 	document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" >\n  <div id="filterField" class="standardContent">');
     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-    document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div></div>\n </div> \n');
+    document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div>\n </div> \n');
     document.write('<div class="noResultsToShow standardContent">' + message + '</div>\n');
-    document.write('</div>');
+    document.write('</div></div>');
 
   }
   if (fieldZone == 'ZoneB'){
@@ -29,10 +29,11 @@ try {
 
 	document.write('<div id="filterFieldWrapper" class="contentItem vspace hspace" data-posistion-default="ZoneB" data-position-selected="ZoneB" role="group" aria-label="search and filter">\n  <div id="filterField">');
     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-    document.write(fieldFieldCode + ' </div> \n');
-    //document.write('<div class="noResultsToShow">' + message + '</div>\n');
-    document.write('</div>');
+    document.write('<div>\n ' +fieldFieldCode + ' </div> \n');
+    document.write('<div class="noResultsToShow">' + message + '</div>\n');
+    document.write('</div></div>');
   }
+
 } catch(err) {
 	document.write(err.message);
 }
