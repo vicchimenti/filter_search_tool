898cce3201b98a30f29871f738d8137cd05793b8
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-23 16:43:41 -0700

    almost

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -105,13 +105,15 @@ try {
   /* -- Write all the things -- */
   // <i class="fas fa-minus"></i>
   // var el = document.getElementsByClassName('educationAbroadItemWrapper')[0];
-  // var idx = this.id;
+  // var idx = el.id;
+
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
-  document.write('<button class="btn btn-info collapsed" type="button" data-toggle="collapse" data-target="id<t4 type="meta" meta="content_id" />" aria-expanded="false">');
+  var idx = '<t4 type="meta" meta="content_id" />';
+  document.write('<button class="btn btn-info collapsed" type="button" data-toggle="collapse" data-target="id'+idx+'">');
   document.write('<span><h3>' + program + '</h3>\n');
   document.write('<div class="descriptionWrapper">' + generalDescription + '</div></span>');
   document.write('<i class="fas fa-plus"></i></button>')
-  document.write('<div class="detailWrapper" id="id<t4 type="meta" meta="content_id" />">')
+  document.write('<div class="detailWrapper" id="id'+idx+'">')
   document.write('<div class="destination"><h4>Destinations</h4><ul>' +  listOfDestinations + '</ul></div>');
   document.write('<div class="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
   document.write('<div class="programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
