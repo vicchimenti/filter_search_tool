7754736f509da7856e6fa842b9e64838b6b7d725
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-24 09:33:15 -0700

    Update educationAbroadProgram.js

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -111,12 +111,12 @@ try {
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
   document.write('<div class="card">');
   document.write('<div class="card-header" id="heading<t4 type=\'meta\' meta=\'content_id\' />">');
-  document.write('<button class="btn btn-info collapsed" data-toggle="collapse" data-target="collapse<t4 type=\'meta\' meta=\'content_id\' />" aria-expanded="false" aria-controls="collapse<t4 type=\'meta\' meta=\'content_id\' />">');
+  document.write('<button class="btn btn-info collapsed" type="button" data-toggle="collapse" data-target="#collapse<t4 type=\'meta\' meta=\'content_id\' />" aria-expanded="false" aria-controls="collapse<t4 type=\'meta\' meta=\'content_id\' />">');
   document.write('<h3>' + program + '</h3>\n');
   document.write('<div class="descriptionWrapper">' + generalDescription + '</div>');
   document.write('<i class="fas fa-plus"></i><i class="fas fa-minus"></i></button>');
   document.write('</div>');
-  document.write('<div id="collapse<t4 type=\'meta\' meta=\'content_id\' />" class="collapse" aria-labelledby="heading<t4 type=\'meta\' meta=\'content_id\' />" data-parent="#accordion">');
+  document.write('<div id="collapse<t4 type=\'meta\' meta=\'content_id\' />" class="collapse" aria-labelledby="heading<t4 type=\'meta\' meta=\'content_id\' />" data-parent="#accordion" aria-expanded="false">');
   document.write('<div class="card-body">');
   document.write('<div class="destination"><h4>Destinations</h4><ul>' +  listOfDestinations + '</ul></div>');
   document.write('<div class="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
