7d04885955120ca135f55a2e6dc808cf62e790bf
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-07-25 15:02:43 -0700

    Update educationAbroadProgram.js

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -119,17 +119,17 @@ try {
   /* -- Write all the collapsible body -- */
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, collapseDiv));
   document.write('<div class="card-body">');
-  document.write('<div class="destination"><h4>Destinations</h4><ul>' +  listOfDestinations + '</ul></div>');
+  document.write('<div class="destination"><h3>Destinations</h3><ul>' +  listOfDestinations + '</ul></div>');
+  document.write('<div class="programImage"><img src="'+ programImageMedia +'" /></div>');
   document.write('<div class="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
   document.write('<div class="programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
-  document.write('<div class="programImage"><img src="'+ programImageMedia +'" /></div>'); // ********* test and update image link if necessary ************
-  document.write('<div class="externalLink"><a class="programItemExternalWebsiteLink" target="_blank" href="' + externalWebsiteLink + '">Visit Website<span class="fas fa-external-link-alt"></span></a></div>');
   document.write('<div class="fieldOfStudy"><h4>Field of Study</h4><ul>' +  listOfStudyAreas + '</ul></div>');
   document.write('<div class="features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
   document.write('<div class="programType"><h4>Program Type</h4><ul> ' +  listOfTypes + '</ul></div>');
   document.write('<div class="language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
   document.write('<div class="housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
   document.write('<div class="gpa"><h4>GPA Requirements</h4><ul> ' +  listOfGPA + '</ul></div>');
+  document.write('<div class="externalLink"><a class="programItemExternalWebsiteLink" target="_blank" href="' + externalWebsiteLink + '">Visit' + program + ' Website<span class="fas fa-external-link-alt"></span></a></div>');
 
   /* -- Write Closing Tags -- */
   document.write('</div></div></div>');
