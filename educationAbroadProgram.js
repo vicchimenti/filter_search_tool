67820449fc5f20dbbf30ea55e4fc7735b308ffac
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-08-13 09:34:33 -0700

    ud

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -113,7 +113,7 @@ try {
   document.write('<div class="card">');
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, cardHeader));
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, buttonLink));
-  document.write('<span><i class="fas fa-minus"></i><i class="fas fa-plus"></i>' + program + '</span></button>\n');
+  document.write('<span class="programTitleBox"><i class="fas fa-minus"></i><i class="fas fa-plus"></i><span class="programTitle">' + program + '</span></span></button>\n');
   document.write('<div class="descriptionWrapper">' + generalDescription + '</div>');
   document.write('</div>');
 
