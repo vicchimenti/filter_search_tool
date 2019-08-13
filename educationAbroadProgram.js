a26eb32ca910a1adddf96f7adcfb72be994fa47d
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-08-13 11:49:51 -0700

    attempt nested container fluid in card body

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -127,28 +127,48 @@ try {
   document.write('</div>');
   /* -- Write Program Cards--*/
   document.write('<div class="card-body" id="programBody">');
-  document.write('<div class="card-group">');
+  document.write('<div class="container-fluid">');
   /* -- Write Program Level 1 Summary Stats --*/
-  document.write('<div class="card" id="card1">');
-  document.write('<div class="card-body" id="level1">');
-  document.write('<div class="destination"><h3>Destinations</h3><ul>' +  listOfDestinations + '</ul></div>');
-  document.write('<div class="programType"><h4>Program Type</h4><ul> ' +  listOfTypes + '</ul></div>');
-  document.write('<div class="fieldOfStudy"><h4>Field of Study</h4><ul>' +  listOfStudyAreas + '</ul></div>');
-  document.write('</div></div>');
+  document.write('<div class="row no-gutters" id="row1">');
+  document.write('<div class="col-md-4" id="destination"><h3>Destinations</h3><ul>' +  listOfDestinations + '</ul></div>');
+  document.write('<div class="col-md-4" id="programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
+  document.write('<div class="col-md-4" id="gpa"><h4>GPA Requirements</h4><ul> ' +  listOfGPA + '</ul></div>');
+  document.write('</div>');
   /* -- Write Program Level 2 Summary Stats --*/
-  document.write('<div class="card" id="card2">');
-  document.write('<div class="card-body" id="level2">');
-  document.write('<div class="programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
-  document.write('<div class="language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
-  document.write('<div class="features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
-  document.write('</div></div>');
+  document.write('<div class="row no-gutters" id="row1">');
+  document.write('<div class="col-md-4" id="programType"><h4>Program Type</h4><ul> ' +  listOfTypes + '</ul></div>');
+  document.write('<div class="col-md-4" id="language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
+  document.write('<div class="col-md-4" id="housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
+  document.write('</div>');
   /* -- Write Program Level 3 Details --*/
-  document.write('<div class="card" id="card3">');
-  document.write('<div class="card-body" id="level3">');
-  document.write('<div class="gpa"><h4>GPA Requirements</h4><ul> ' +  listOfGPA + '</ul></div>');
-  document.write('<div class="housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
-  document.write('<div class="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
-  document.write('</div></div>');
+  document.write('<div class="row no-gutters" id="row1">');
+  document.write('<div class="col-md-4" id="fieldOfStudy"><h4>Field of Study</h4><ul>' +  listOfStudyAreas + '</ul></div>');
+  document.write('<div class="col-md-4" id="features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
+  document.write('<div class="col-md-4" id="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
+  document.write('</div>');
+
+  // document.write('<div class="card-group">');
+  // /* -- Write Program Level 1 Summary Stats --*/
+  // document.write('<div class="card" id="card1">');
+  // document.write('<div class="card-body" id="level1">');
+  // document.write('<div class="destination"><h3>Destinations</h3><ul>' +  listOfDestinations + '</ul></div>');
+  // document.write('<div class="programType"><h4>Program Type</h4><ul> ' +  listOfTypes + '</ul></div>');
+  // document.write('<div class="fieldOfStudy"><h4>Field of Study</h4><ul>' +  listOfStudyAreas + '</ul></div>');
+  // document.write('</div></div>');
+  // /* -- Write Program Level 2 Summary Stats --*/
+  // document.write('<div class="card" id="card2">');
+  // document.write('<div class="card-body" id="level2">');
+  // document.write('<div class="programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
+  // document.write('<div class="language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
+  // document.write('<div class="features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
+  // document.write('</div></div>');
+  // /* -- Write Program Level 3 Details --*/
+  // document.write('<div class="card" id="card3">');
+  // document.write('<div class="card-body" id="level3">');
+  // document.write('<div class="gpa"><h4>GPA Requirements</h4><ul> ' +  listOfGPA + '</ul></div>');
+  // document.write('<div class="housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
+  // document.write('<div class="term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
+  // document.write('</div></div>');
 
   /* -- Write Closing Tags -- */
   document.write('</div></div></div></div>');
