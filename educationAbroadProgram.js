941d24efb1cd69e2ec719347989e130798615989
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-08-13 13:31:12 -0700

    remove logic from image div

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -129,9 +129,7 @@ try {
   /* -- Write Program Image and Link--*/
   document.write('<div class="row no-gutters" id="image-link">');
   document.write('<div class="col-md-12" id="image-link">');
-  if (programImageMedia) {
-      document.write('<div class="card-img-top"><img src="'+ programImageMedia +'" alt="'+altImage+'"/></div>');
-  }
+  document.write('<div class="card-img-top"><img src="'+ programImageMedia +'" alt="'+altImage+'"/></div>');
   if (externalWebsiteLink != "") {
       document.write('<div class="externalLink"><a class="programItemExternalWebsiteLink" target="_blank" href="' + externalWebsiteLink + '"><h5>Visit ' + program + ' Website</h5></div>');
   }
