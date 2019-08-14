313c9be9ef79e3df9013cb98cb2634130340fd87
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-08-14 12:59:28 -0700

    layout foundation done

--- educationAbroadProgram.js
+++ educationAbroadProgram.js
@@ -143,16 +143,20 @@ try {
   document.write('<div class="col-md-3 programDetails programFee"><h4>Program Fees</h4><ul>' +  listOfProgramFees + '</ul></div>');
   document.write('<div class="col-md-3 programDetails gpa"><h4>GPA Requirements</h4><ul> ' +  listOfGPA + '</ul></div>');
   document.write('<div class="col-md-3 programDetails programType"><h4>Program Type</h4><ul> ' +  listOfTypes + '</ul></div>');
+
   document.write('</div>');
   /* -- Write Program Level 2 Summary Stats --*/
   document.write('<div class="row no-gutters" id="row2">');
-  document.write('<div class="col-md-3 programDetails language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
-  document.write('<div class="col-md-3 programDetails housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
-  document.write('<div class="col-md-3 programDetails term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
-  document.write('<div class="col-md-3 programDetails features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
+  document.write('<div class="col-md-4 programDetails housing"><h4>Housing Accommodations</h4><ul> ' +  listOfHousing + '</ul></div>');
+  document.write('<div class="col-md-4 programDetails language"><h4>Language of Instruction</h4><ul>' +  listOfLanguages + '</ul></div>');
+  document.write('<div class="col-md-4 programDetails term"><h4>Term</h4><ul>' +  listOfTerms + '</ul></div>');
   document.write('</div>');
   /* -- Write Program Level 3 Details --*/
   document.write('<div class="row no-gutters" id="row3">');
+  document.write('<div class="col-md-12 programDetails features"><h4>Features</h4><ul>' +  listOfFeatures + '</ul></div>');
+  document.write('</div>');
+  /* -- Write Program Level 4 Details --*/
+  document.write('<div class="row no-gutters" id="row4">');
   document.write('<div class="col-md-12 programDetails fieldOfStudy"><h4>Field of Study</h4><ul>' +  listOfStudyAreas + '</ul></div>');
   document.write('</div>');
 
