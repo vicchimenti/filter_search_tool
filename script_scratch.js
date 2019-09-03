63ba09e4bcfe769540293e7dffaf006a3500de90
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-03 11:34:05 -0700

    ud

--- script_scratch.js
+++ script_scratch.js
@@ -1,34 +1,34 @@
 <script>
 $(function () {
     console.log("one");
-    $('#filterField').addClass('whoopie1');
-    console.log("one filterField");
+    $('.js-t4form-container').addClass('whoopie1');
+    console.log("one js-t4form-container");
 });
 $(function () {
     console.log("two");
-    $('.js-t4form-container').addClass('whoopie2');
-    console.log("two js-t4form-container");
+    $('.js-t4form-container').contents().find('.js-elements-container').addClass('whoopie2');
+    console.log("two js-t4form-container contents find js-elements-container");
 });
 $(function () {
     console.log("three");
-    $('.js-main-container').contents().find('.div').addClass('whoopie3');
-    console.log("three js-main-container contents find");
+    $('.js-t4form-container iframe').contents().find('.js-main-container').addClass('whoopie3');
+    console.log("three js-t4form-container iframe contents find js-main-container");
 });
 $(function () {
     console.log("four");
-    $('.js-t4form-container iframe').contents().find('.js-main-container').addClass('whoopie4');
-    console.log("four js-t4form-container iframe contents find js-main-container");
+    $('.js-main-container iframe').contents().find('.div').addClass('whoopie4');
+    console.log("four js-main-container iframe contents find");
 });
 $(function () {
     console.log("five");
-    $('.js-elements-container').contents().addClass('whoopie5');
-    console.log("five .js-elements-container contents");
+    $('.js-elements-container iframe').contents().find('.div').addClass('whoopie5');
+    console.log("five .js-elements-container iframe contents find");
 });
-$(function (){
+$(function () {
     console.log("six");
-    setInterval(function() {
-        parent.postMessage("Hello", "formbankUri");
+    setInterval(function () {
+        parent.postMessage("Hello", "https://us.formbank.us");
     }, 1000);
-    console.log("six postMessage formbankUri");
+    console.log("six postMessage https://us.formbank.us");
 });
 </script>
