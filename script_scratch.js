c6286a318ddf438da4ed3be9f847defbb0bc428f
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-04 12:01:35 -0700

    Update script_scratch.js

--- script_scratch.js
+++ script_scratch.js
@@ -2,44 +2,45 @@
 $(function () {
     console.log("one");
     $('.js-t4form-container').addClass('whoopie1');
-    console.log("one js-t4form-container 0");
+    console.log("one js-t4form-container whoopie1 0");
 });
 $(function () {
     console.log("two");
     setTimeout(function () {
         $('.js-main-container').addClass('whoopie2');
     }, 4000);
-    console.log("two js-main-container 4000");
+    console.log("two js-main-container whoopie2 4000");
 });
 $(function () {
     console.log("three");
     setTimeout(function () {
         $('.js-elements-container').addClass('whoopie3');
     }, 5000);
-    console.log("three js-elements-container 5000");
+    console.log("three js-elements-container whoopie3 5000");
 });
 $(function () {
     console.log("four");
     setTimeout(function() {
-        $('.fieldOfStudyDropDownMenu').addClass('whoopie4');
+        $('.form-group').addClass('whoopie4');
     }, 5000);
-    console.log("four fieldOfStudyDropDownMenu 5000");
+    console.log("four form-group whoopie4 5000");
 });
 $(function () {
     console.log("five");
     $(window).load(function () {
-        $('.js-main-container').delay(5000).addClass('onload1');
-    });
-    console.log("five js-main-container window load delay 5000");
+        setTimeout(function (){
+            $('.js-main-container').addClass('onload1');
+        }, 2000);    });
+    console.log("five js-main-container window onload1 delay 2000");
 });
 $(function () {
     console.log("six");
     $(window).load(function () {
         setTimeout(function (){
             $('.js-elements-container').addClass('onload2');
-        }, 1500);
+        }, 2000);
     });
-    console.log("six js-elements-container window load setTimeout 1500");
+    console.log("six js-elements-container window onload2 setTimeout 2000");
 });
 
 </script>
