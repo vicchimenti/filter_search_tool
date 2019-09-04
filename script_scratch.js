6d79de27588f778d7308f1b28cfb69b8405a99fc
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-04 11:09:45 -0700

    form-group 4000 works

--- script_scratch.js
+++ script_scratch.js
@@ -2,33 +2,27 @@
 $(function () {
     console.log("one");
     $('.js-t4form-container').addClass('whoopie1');
-    console.log("one js-t4form-container");
+    console.log("one js-t4form-container 0");
 });
 $(function () {
     console.log("two");
-    $('.js-t4form-container').delay(5000).find('.form-group').addClass('whoopie2');
-    console.log("two form-group 2000");
+    setTimeout(function () {
+        $('.js-main-container').addClass('whoopie2');
+    }, 1000);
+    console.log("two js-main-container 1000");
 });
 $(function () {
     console.log("three");
-    $('.js-main-container').delay(6000).addClass('whoopie3');
-    console.log("three js-main-container 3000");
+    setTimeout(function () {
+        $('.js-elements-container').addClass('whoopie3');
+    }, 3000);
+    console.log("three js-elements-container 3000");
 });
 $(function () {
     console.log("four");
-    $('.js-elements-container').delay(7000).addClass('whoopie4');
-    console.log("four js-elements-container 4000");
-});
-$(function () {
-    console.log("five");
     setTimeout(function() {
-        $('.js-main-container').addClass('timeout1');
+        $('.form-group').addClass('whoopie4');
     }, 4000);
-    console.log("five jquery setTimeout");
+    console.log("four form-group 4000");
 });
-setTimeout(function() {
-    console.log("six");
-    $('.js-main-container').addClass('timeout2');
-    console.log("six raw js");
-}, 4000);
 </script>
