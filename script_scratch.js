c92289223b4f7026353f72fcf42906a39f97bc1d
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-04 11:21:29 -0700

    settimeout working at 5000

--- script_scratch.js
+++ script_scratch.js
@@ -8,21 +8,38 @@ $(function () {
     console.log("two");
     setTimeout(function () {
         $('.js-main-container').addClass('whoopie2');
-    }, 1000);
-    console.log("two js-main-container 1000");
+    }, 3000);
+    console.log("two js-main-container 3000");
 });
 $(function () {
     console.log("three");
     setTimeout(function () {
         $('.js-elements-container').addClass('whoopie3');
-    }, 3000);
-    console.log("three js-elements-container 3000");
+    }, 5000);
+    console.log("three js-elements-container 5000");
 });
 $(function () {
     console.log("four");
     setTimeout(function() {
         $('.form-group').addClass('whoopie4');
-    }, 4000);
-    console.log("four form-group 4000");
+    }, 5000);
+    console.log("four form-group 5000");
 });
+$(function () {
+    console.log("five");
+    $(window).load(function () {
+        $('.js-main-container').delay(3000).addClass('onload1');
+    });
+    console.log("five js-main-container window load delay 3000");
+});
+$(function () {
+    console.log("six");
+    $(window).load(function () {
+        setTimeout(function (){
+            ('.js-elements-container').addClass('onload1');
+        }, 2000);
+    });
+    console.log("six js-elements-container window load setTimeout 2000");
+});
+
 </script>
