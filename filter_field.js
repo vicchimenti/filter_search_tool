50055fb28ce652fab8fd4227cb4ba821bb73c378
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-05 09:26:25 -0700

    Update filter_field.js

--- filter_field.js
+++ filter_field.js
@@ -1,6 +1,6 @@
 try {
   var fieldInstructions = content.get("Instructions");
-  var fieldFieldCode = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='form' name='Education Abroad Search Filters' id='2162' />");
+  var fieldFieldCode = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='form' name='Education Abroad Search Filters' id='2236' />");
   var fieldNoResultsMessage = content.get("No Results Message");
   var message = "";
   var fieldZone = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Zone' output='normal' display_field='value' />");
@@ -15,7 +15,7 @@ try {
 
 	document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" >\n  <div id="filterField" class="standardContent">');
     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-    document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div>\n </div> \n');
+    document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div>\n</div>\n');
     document.write('<div class="noResultsToShow standardContent">' + message + '</div>\n');
     document.write('</div></div>');
 
@@ -27,9 +27,9 @@ try {
       message = fieldNoResultsMessage;
     }
 
-	document.write('<div id="filterFieldWrapper" class="contentItem vspace hspace" data-posistion-default="ZoneB" data-position-selected="ZoneB" role="group" aria-label="search and filter">\n  <div id="filterField">');
+	document.write('<div id="filterFieldWrapper" class="contentItem vspace hspace" data-posistion-default="ZoneB" data-position-selected="ZoneB" role="group" aria-label="search and filter">\n<div id="filterField">');
     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-    document.write('<div>\n ' +fieldFieldCode + ' </div> \n');
+    document.write('<div>\n ' +fieldFieldCode + ' </div>\n');
     document.write('<div class="noResultsToShow">' + message + '</div>\n');
     document.write('</div></div>');
   }
