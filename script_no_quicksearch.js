992f5229a4504d7f90056a00cce143a1c9514f44
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-10 09:54:22 -0700

    rewite keyword from scratch

--- script_no_quicksearch.js
+++ script_no_quicksearch.js
@@ -6,25 +6,6 @@ $(function () {
     $(window).load(function () {
         setTimeout(function (){
             $(function () {
-              $('#id_search').quicksearch('.educationAbroadItemWrapper', {
-                'delay': 400,
-                'selector': ['h3, p'],
-                'stripeRows': ['even', 'odd'],
-                'noResults': '.noResultsToShow',
-                'bind': 'keyup click',
-                'minValLength': 2,
-                'prepareQuery': function (val) {
-                  return new RegExp(val, "i");
-                },
-                'testQuery': function (query, txt, _row) {
-                  return query.test(txt);
-                },
-                'show': function() {
-                  $(this).removeClass('hideByTextbox');
-                },
-                'hide': function() {
-                  $(this).addClass('hideByTextbox');
-                }
               });
             });
         }, 2000);
