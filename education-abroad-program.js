2e28af6f95e4d3833132285c4b14675b19063871
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-16 13:35:33 -0700

    remove aria label

--- education-abroad-program.js
+++ education-abroad-program.js
@@ -117,7 +117,7 @@ try {
   /*  -- Parse out Runtime Generated Content IDs -- */
   var cardHeader = '<div class="card-header" id="heading<t4 type=\'meta\' meta=\'content_id\' />">';
   var buttonLink = '<button class="btn btn-link" type="button" id="button<t4 type=\'meta\' meta=\'content_id\' />" data-toggle="collapse" data-target="#collapse<t4 type=\'meta\' meta=\'content_id\' />" aria-expanded="false" aria-controls="collapse<t4 type=\'meta\' meta=\'content_id\' />">';
-  var collapseDiv = '<div class="collapse" id="collapse<t4 type=\'meta\' meta=\'content_id\' />" aria-labelledby="heading<t4 type=\'meta\' meta=\'content_id\' />" data-parent="id<t4 type=\'meta\' meta=\'content_id\' />">';
+  var collapseDiv = '<div class="collapse" id="collapse<t4 type=\'meta\' meta=\'content_id\' />">';
 
   /* -- Write all the collapsible header -- */
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
