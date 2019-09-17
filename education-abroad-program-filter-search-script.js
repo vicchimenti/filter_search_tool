1fa0bcfe40e9d097683a3d4ba44f89dd70aaaeab
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-17 13:09:56 -0700

    Revert "Update education-abroad-program-filter-search-script.js"
    This reverts commit db9faf195431b41f6ccc4ecc0e52a2928a4e0b3e.

--- education-abroad-program-filter-search-script.js
+++ education-abroad-program-filter-search-script.js
@@ -73,9 +73,9 @@ $(function () {
     $(window).load(function () {
         // Once window loads set a timeout delay
         setTimeout(function () {
-            $(function() {SelectBox-ByTerm
+            $(function() {
                 // When the Dropdown Menu Selector Academic Terms Change - Execute change function
-                $('#SelectBox-ByDestination').change(function () {
+                $('#input-28833').change(function () {
                     // T4 List Values Must Parse Out Extra Characters
                     var rawFormValue = $(this).val();
                     // The T4 Form generates a specific Delimiter to Use
@@ -122,8 +122,8 @@ $(function () {
 $(function () {
     $(window).load(function () {
         setTimeout(function () {
-            $(function() {SelectBox-ByProgramFee
-                $('#SelectBox-ByTerm').change(function () {
+            $(function() {
+                $('#input-28834').change(function () {
                     var rawFormValue = $(this).val();
                     var parsedValue = rawFormValue.split('-:::-:::-');
                     var key = parsedValue[1];
@@ -185,7 +185,7 @@ $(function () {
     $(window).load(function () {
         setTimeout(function () {
             $(function() {
-                $('#SelectBox-ByDestination').change(function () {
+                $('#input-28832').change(function () {
                     var rawFormValue = $(this).val();
                     var parsedValue = rawFormValue.split('-:::-:::-');
                     var key = parsedValue[1];
