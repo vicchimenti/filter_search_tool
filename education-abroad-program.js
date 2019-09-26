0c2e5ee2848d33078fddd7792044bbf56278b0a2
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-09-26 12:04:30 -0700

    rm card img top

--- education-abroad-program.js
+++ education-abroad-program.js
@@ -134,7 +134,7 @@ try {
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, buttonLink));
   document.write('<span class="programTitleBox"><i class="fas fa-minus"></i><i class="fas fa-plus"></i><span class="programTitle">' + program + '</span></span></button>\n');
   document.write('<div class="programCity"><h5>' + city + '</h5></div>');
-  document.write('<div class="descriptionWrapper"><div class="card-img-top"><img src="' + programImageMedia + '" alt="' + altImage + '" class="programImage" /></div><p class="programDescription">' + generalDescription + '</p></div>');
+  document.write('<div class="descriptionWrapper"><img src="' + programImageMedia + '" alt="' + altImage + '" class="programImage" /><p class="programDescription">' + generalDescription + '</p></div>');
 
   /* -- Write the collapsible body -- */
   document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, collapseDiv));
